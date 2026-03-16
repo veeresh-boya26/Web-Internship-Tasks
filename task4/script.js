18779814eb668e5250e80438478e2303331b4536
B Veeresh <veereshnaidu2623@gmail.com>
2026-03-16 12:37:07 +0530

    Add files via upload

---
+++ task4/script.js
@@ -0,0 +1,16 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll("nav a").forEach(link => {
+
+link.addEventListener("click", function(e){
+e.preventDefault();
+
+const target = document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+behavior: "smooth"
+});
+
+});
+
+});
